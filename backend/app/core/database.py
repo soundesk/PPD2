c2c3b5a989245489074979a137225fef6c6e6398
soundes kohil <soundes.kohil.ubma@gmail.com>
2025-12-31 17:08:14 +0100

    Use environment variable for DATABASE_URL
    Refactor database connection to use environment variable for DATABASE_URL.

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -2,24 +2,17 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# --------------------------------------------------
-# 1. CONFIGURATION DE LA CONNEXION A POSTGRESQL
-# --------------------------------------------------
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASE_URL = os.getenv("postgresql://postpartum_bdd_user:U85XfstEvPsJPk2ayLVBcdkBO0PQmTo0@dpg-d5afukpr0fns738as3eg-a.virginia-postgres.render.com/postpartum_bdd")  # <-- use env variable
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is not set")
 
-# create_engine → crée la connexion
 engine = create_engine(DATABASE_URL, echo=True)
 
-# sessionmaker → pour créer des sessions (interaction DB)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Base → classe mère des modèles SQLAlchemy
 Base = declarative_base()
 
-# --------------------------------------------------
-# 2. FONCTION POUR OBTENIR UNE SESSION
-# --------------------------------------------------
 def get_db():
     db = SessionLocal()
     try:
